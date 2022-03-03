d05bea2e4e4420deb11fb95009c31d8878b0fbd9
juanDaRestrepo <jdrestrepo@unitecnica.net>
2022-03-03 18:15:42 -0500

    configuring .env instalation of dovenv

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola mundo!!")
+const express = require('express');
+require('dotenv').config();
+
+// Crear servidor de express
+const app = express();
+
+//Directorio publico
+app.use( express.static('public'));
+
+//rutas
+/* app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+}); */
+
+//Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
+});
